3be72999acf595f11a27b41429749024d5a80022
Khan Schroder-Turner <khan.schroder-turner@ghd.com>
2021-10-23 23:58:24 +1100

    Fleshed out a route

--- imageapi/resources/image.py
+++ imageapi/resources/image.py
@@ -1,6 +1,24 @@
-from pydantic import BaseModel, Field
+import falcon
+import json
+from ..schemas.base_api_spec import api
+from ..schemas.image import ImageSchema
 
-class Image(BaseModel):
-    name: str
-    size: float
-    type: str
+
+class Image:
+    def __init__(self, image_store=None):
+        self._image_store = image_store
+
+    @api.validate(json=ImageSchema)
+    def on_get(self, req, resp, **params):
+        """
+        get an image resource
+
+        returns an image with a specified ID
+        """
+        doc = {
+            "name": "test",
+            "size":1024,
+            "ImageType": "image/png"
+        }
+        resp.text = json.dumps(doc, ensure_ascii=True)
+        resp.status = falcon.HTTP_200
