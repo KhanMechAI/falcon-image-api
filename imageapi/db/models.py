83bedde77343644f20be65bde1131a26b9c147be
Khan Schroder-Turner <khan.schroder-turner@ghd.com>
2021-10-24 10:41:52 +1100

    Building out session and resource tables

--- imageapi/db/models.py
+++ imageapi/db/models.py
@@ -1,39 +1,82 @@
+import uuid
+
 import falcon_sqla
-import sqlalchemy as sa
-from sqlalchemy import Column, ForeignKeyConstraint, Integer, String, UniqueConstraint, create_engine
+from sqlalchemy import Column, Float, ForeignKeyConstraint, Integer, String, create_engine, types
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.types import DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.types import CHAR, TypeDecorator
+from werkzeug.security import check_password_hash, generate_password_hash
 
-from werkzeug.security import generate_password_hash, check_password_hash
-
-engine = create_engine('dialect+driver://my/database')
+engine = create_engine("dialect+driver://my/database")  # ToDo
 manager = falcon_sqla.Manager(engine)
 
 Base = declarative_base()
 
 
+class ChoiceType(types.TypeDecorator):
+    ''' Use this class to create multiple choices for a model'''
+    impl = types.String
+
+    def __init__(self, choices, **kw):
+        self.choices = dict(choices)
+        super(ChoiceType, self).__init__(**kw)
+
+    def process_bind_param(self, value, dialect):
+        return [k for k, v in self.choices.items() if v == value][0]
+
+    def process_result_value(self, value, dialect):
+        return self.choices[value]
+
+
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = "user"
     __table_args__ = (
-        ForeignKeyConstraint(['id'], ['remote_table.id']),
-        {'autoload': True}
+        ForeignKeyConstraint(["id"], ["session.id"]),
+        {"autoload": True}
     )
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String)
     password_hash = Column(String)
+    timestamp_created = Column(Float, server_default=func.utcnow())
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+        self.password_hash = generate_password_hash(password, method="pbkdf2:sha256", salt_length=16)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
 
 class Session(Base):
-    __tablename__ = 'session'
+    __tablename__ = "session"
     __table_args__ = (
-        ForeignKeyConstraint(['id'], ['remote_table.id']),
-        UniqueConstraint('foo'),
-        {'autoload': True}
+        {"autoload": True}
     )
+
+
+class Usage(Base):
+    __tablename__ = "usage"
+    __table_args__ = (
+        ForeignKeyConstraint(["id"], ["remote_table.id"]),
+        {"autoload": True}
+    )
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    timestamp = Column(Float, server_default=func.utcnow())
+    user_id = ForeignKey()
+    resource = Column(ChoiceType())  # ToDo
+    response = Column()  # ToDo
+
+
+class Tags(Base):
+    __tablename__ = "tags"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tag = Column(String)
+
+
+class Images(Base):
+    __tablename__ = "images"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    image_type = Column() # ToDo add choices
