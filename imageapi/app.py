b56913998cd01b1cbed5c9990fea0f0e2cd61d0f
Khan Schroder-Turner <khan.schroder-turner@ghd.com>
2021-10-24 15:26:32 +1100

    updating config and app.py. errors out due to import errors. needs refactor. coming soon

--- imageapi/app.py
+++ imageapi/app.py
@@ -1,11 +1,18 @@
 import falcon
-from middleware.jwt_checker import jwt_checker
+from .middleware.jwt_checker import jwt_checker
 from .resources.image import Image
+from .db.manager import init_db
+import dynamic_yaml
 
+with open('config.yaml') as conf:
+    config = dynamic_yaml.load(conf)
+
+manager = init_db(config.databases.dev.sqlite)
 
 app = application = falcon.App(
     middleware=[
         jwt_checker,
+        manager
     ]
 )
 
@@ -16,3 +23,12 @@ app.add_route('/image', image)
 from .schemas.base_api_spec import api
 
 api.register(app)
+
+if __name__ == "__main__":
+    from wsgiref.simple_server import make_server
+
+    with make_server('', 8080, app) as httpd:
+        print('Serving on port 8080...')
+
+        # Serve until process is killed
+        httpd.serve_forever()
